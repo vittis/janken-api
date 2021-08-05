0cd94165b01656c038b4615c9c675389efca48e8
Vitor <vitbolb1@gmail.com>
2021-08-05 20:44:18 -0300

    Change cors

--- app.ts
+++ app.ts
@@ -15,7 +15,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://jankenio.netlify.app/',
     credentials: true
   }
 });
